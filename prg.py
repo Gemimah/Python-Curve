cae2d6e8e67ec20880fae8d0243f48a360c9f2f1
Gemimah <mugishagemimah@gmail.com>
2025-09-09 08:27:59 +0200

    functions explained

---
+++ prg.py
@@ -0,0 +1,33 @@
+# User defined functions
+
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+    
+    greet("Mosh", "Hamedani")
+    greet("John","Smith")
+    
+    #Types of functions
+    #1- Perform a task
+    #2- Return a value
+    
+    def greet(name):
+        print(f"Hi {name}")
+        
+        def get_greeting(name):
+            return f"Hi {name}"
+        
+        message = get_greeting("Mosh")
+        file = open("context.txt", "w")
+        file.write(message)
+        
+        def increment(number, by=1):
+            return number +by
+        print(increment(2))
+        
+        
+    
+
+
+
+        
