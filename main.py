ac210c42691936917d5f6e565115ab91be6e9ea0
Gemimah <mugishagemimah@gmail.com>
2025-09-09 21:18:27 +0200

    typecasting explained

---
+++ main.py
@@ -0,0 +1,16 @@
+#Typecasting = the process of converting a variable from one data type to another
+# str(), int(),float(),bool()
+
+name = "Gemimah"
+age = 25
+gpa= 3.2
+is_student = True
+
+print(type(is_student))
+
+gpa = int(gpa)
+
+age = str(age)
+print(type(age))
+name = bool(name)
+print(name)
