7ca1fddebf630bf8a6b9693503365141067d2fb9
Gemimah <mugishagemimah@gmail.com>
2025-09-09 21:52:09 +0200

    exercises explained

--- main.py
+++ main.py
@@ -14,3 +14,31 @@ age = str(age)
 print(type(age))
 name = bool(name)
 print(name)
+
+#input() = A function that prompts the user to enter data and returns the entered data as a string
+
+name = input("What is your name?: ")
+
+age = input("How old is you?: ")
+age = int(age)
+age = age +1
+print(f"Hello {name}")
+print("HAPPY BIRTHDAY !")
+print(f"You are {age} years old")
+
+# Area of a rectangle
+
+length =float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+area = length * width
+
+print(f"The area is:{area} cm")
+
+# Shopping Cart Program
+
+item = input("What item would you like: ")
+price = input("What is the price?: ")
+quantity = input("How many would you like?: ")
+
+total= price*quantity
+print(total)
