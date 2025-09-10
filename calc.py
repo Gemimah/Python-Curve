f0a8e6055e1d90be171f1db93acca98ea4306ced
Gemimah <mugishagemimah@gmail.com>
2025-09-10 08:15:53 +0200

    built In functions

---
+++ calc.py
@@ -0,0 +1,16 @@
+#built in Math related functions
+x = 3.14
+y = 4
+z = 5
+#result = round(x)
+#result = abs(y)
+#result = pow(4,3)
+#result = max(x,y,z)
+#result = min(x,y,z)
+
+
+result = max(x,y,z)
+result1 = min(x,y,z)
+
+print(result)
+print(result1)
