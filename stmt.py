d630dd973c399604561686b4e47e9ec6c53e0208
Gemimah <mugishagemimah@gmail.com>
2025-09-10 14:52:17 +0200

    stmt explained

---
+++ stmt.py
@@ -0,0 +1,12 @@
+#if = DO some code only IF some condition is True
+
+age = int(input("Enter your age: "))
+
+if age >= 100:
+    print("you are too old to sign11 up!")
+elif age <0 :
+    print("You haven't been born yet!")
+elif age >=100:
+    print
+else:
+    print("You must be 18+ to sign up")
