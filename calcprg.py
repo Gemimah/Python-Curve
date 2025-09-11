8d799910c3739a626d4f13a3f6c992c8c5d23662
Gemimah <mugishagemimah@gmail.com>
2025-09-11 08:26:56 +0200

    Timer program

--- calcprg.py
+++ calcprg.py
@@ -1,6 +1,6 @@
 #python calculator
 
-operator = input("Enter an operator(+ - * /): ")
+"""operator = input("Enter an operator(+ - * /): ")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
@@ -18,4 +18,17 @@ elif operator == "*":
     
 elif operator == "/":
     result = num1/num2
-    print(result)
+    print(result)"""
+
+
+
+import time
+
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(my_time,0, -1):
+    print(x)
+    time.sleep(1)
+    
+    print()
+    
